f87d398a2180a8ff3a4ba112db32931272ad1c8e
FanidiyaTasya <fanidiyatasya@gmail.com>
2025-06-07 14:05:39 +0700

    fix: correct server port binding for deployment

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const init = async () => {
   }
 
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: Number(process.env.PORT),
     host: '0.0.0.0',
     routes: {
       cors: {
