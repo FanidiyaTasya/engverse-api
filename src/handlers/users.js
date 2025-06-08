b34d392aefa7c0e8008a06b7fc2eb53eed957afe
FanidiyaTasya <fanidiyatasya@gmail.com>
2025-06-08 12:45:08 +0700

    fix: adjust created_at format

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -18,7 +18,7 @@ const usersHandler = {
 
       const hashedPassword = await bcrypt.hash(password, saltRounds);
       const id = nanoid();
-      const createdAt = new Date().toISOString();
+      const createdAt = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
       await pool.execute(
         'INSERT INTO users (id, name, email, password, created_at) VALUES (?, ?, ?, ?, ?)',
